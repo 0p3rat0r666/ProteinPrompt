0f5f091ccfc7ae82aebce7251d115b1221bff97a
X401U1 <106628238+0p3rat0r666@users.noreply.github.com>
2024-11-05 09:55:41 -0600

    Created game.js
    Started making the back bone for the RNA of the game.

---
+++ game.js
@@ -0,0 +1,7 @@
+// prints out the backbone and stores the bases in a variablw
+function back (){
+  console.log("_________________");
+  console.log(" | | | | | | | |");
+}
+const BackBone = back()
+console.log(BackBone);
