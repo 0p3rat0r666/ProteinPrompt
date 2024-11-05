d0b54a85190755dc97c647d5ba1d9f30737e4127
X401U1 <106628238+0p3rat0r666@users.noreply.github.com>
2024-11-05 16:30:44 +0000

    modified:   game.js

--- game.js
+++ game.js
@@ -1,7 +1,20 @@
 // prints out the backbone and stores the bases in a variablw
+// question 1 A U G G U A G C U A A C C U U
+// question 2 C A G G A A U U G C U C G A U
+// question 3 G U A C U C A A G G U C U A G
+// question 4 U U U C A A U C G A C C A A C
+var score = 0;
+var question = [" A U G G U A G C U A A C C U U", " C A G G A A U U G C U C G A U", " G U A C U C A A G G U C U A G", " U U U C A A U C G A C C A A C"];
+
 function back (){
-  console.log("_________________");
-  console.log(" | | | | | | | |");
+  console.log("_______________________________");
+  console.log(" | | | | | | | | | | | | | | |");
+  console.log(question[0]); // change to output the bases for the question
+  // put reminder here maybe?
 }
-const BackBone = back()
-console.log(BackBone);
+
+
+back();
+
+// freaking take answer. this is reminder btw
+console.log("Please look at your chart and type the first three letters of each amino acid IN LOWERCASE. Remember glutamine is gln and glutamate is glu.");
