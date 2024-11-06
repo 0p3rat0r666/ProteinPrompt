60f802106356df8a5461132f04dba11eae871fed
X401U1 <106628238+0p3rat0r666@users.noreply.github.com>
2024-11-06 11:17:56 -0600

    Added system to check if game is completed

--- game.js
+++ game.js
@@ -3,18 +3,28 @@
 // question 2 C A G G A A U U G C U C G A U
 // question 3 G U A C U C A A G G U C U A G
 // question 4 U U U C A A U C G A C C A A C
-var score = 0;
-var question = [" A U G G U A G C U A A C C U U", " C A G G A A U U G C U C G A U", " G U A C U C A A G G U C U A G", " U U U C A A U C G A C C A A C"];
+var score = 4; // set as won for testing
+var question = [" A U G G U A G C U A A C C U U", " C A G G A A U U G C U C G A U", " G U A C U C A A G G U C U A G", " U U U C A A U C G A C C A A C", " "];
 
-function back (){
+function RNA (){
   console.log("_______________________________");
   console.log(" | | | | | | | | | | | | | | |");
   console.log(question[0]); // change to output the bases for the question
-  // put reminder here maybe?
+  if(score === 4){
+    console.log("congradulations! You finished Protein Prompt!! would you like to restart?(write true or false in lowercase)");
+    let restart = prompt();
+    if(restart === "true"){
+      score = 0;
+      RNA();
+    }else{
+      console.log("thanks for playing!");
+    }
+  }else{
+    console.log("Please look at your chart and type the first three letters of each amino acid IN LOWERCASE. Remember glutamine is gln and glutamate is glu.");
+  }
 }
 
 
-back();
+RNA();
 
-// freaking take answer. this is reminder btw
-console.log("Please look at your chart and type the first three letters of each amino acid IN LOWERCASE. Remember glutamine is gln and glutamate is glu.");
+// freaking take answer
